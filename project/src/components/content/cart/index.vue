0fa381d1435ddd9f0919dd2a00cbde3b393f4984
李冰 <8788801+li-bing-a@user.noreply.gitee.com>
2021-04-12 20:08:44 +0800

    购物车列表

--- project/src/components/content/cart/index.vue
+++ project/src/components/content/cart/index.vue
@@ -1,37 +1,114 @@
 <template>
-    <div class="cart">
-        购物车
+  <div class="cart">
+    <div v-for="item in products" :key="item._id">
+      <van-checkbox v-model="item.checked">选中</van-checkbox>
+      <div>
+        <p>{{ item.product.name }}</p>
+        <p>{{ item.product.price }}</p>
+        <p>{{ item.quantity }}</p>
+      </div>
     </div>
+    <van-submit-bar
+      :price="sumPrice * 100"
+      button-text="立即结算"
+      @submit="onSubmit"
+    >
+      <van-checkbox v-model="checked">全选</van-checkbox>
+    </van-submit-bar>
+    <!-- <van-goods-action
+      :price="sumPrice * 100"
+      button-text="立即结算"
+      @submit="onSubmit"
+    >
+      <van-goods-action-icon icon="chat-o" text="客服" dot />
+      <van-goods-action-icon icon="cart-o" text="购物车" badge="5" />
+      <van-goods-action-icon icon="shop-o" text="店铺" badge="12" />
+      <van-goods-action-button type="warning" text="加入购物车" />
+      <van-goods-action-button type="danger" text="立即购买" />
+    </van-goods-action> -->
+  </div>
 </template>
 
 <script>
+import { reqCartlist } from "../api/cart";
+import { Notify } from "vant";
+// import { get } from "../../../utils/request";
+// import { serverUrl } from "../../../utils/common";
+// import { Toast } from "vant";
 export default {
-    
-    components: {},
-    data() {
-        
-        return {
-            
-        };
+  components: {},
+  data() {
+    return {
+      products: [],
+    };
+  },
+  computed: {
+    checked: {
+      // set设置选中状态
+      set(flag) {
+        return this.products.forEach((item) => (item.checked = flag));
+      },
+      get() {
+        // 调用get方法
+        return (
+          this.products.length ==
+          this.products.filter((item) => item.checked == true).length
+        );
+      },
     },
-    methods: {
-        
+    sumPrice() {
+      //计算属性一定必须要有返回值
+      //filter 可以生成新数组 里边存放的是过滤后符合条件的元素
+      return this.products
+        .filter((item) => {
+          //通过过滤，筛选出被选中的商品
+          return item.checked;
+        })
+        .reduce(function(per, cur) {
+          return per + cur.product.price * cur.quantity;
+        }, 0);
     },
-    created() {
-        
+  },
+  watch: {},
+
+  methods: {
+    //   initDetail(id){
+    //         this.$http.get(`http://localhost:3009/api/v1/products/${id}`).then(res=>{
+    //             console.log(res)
+    //             if(res.status === 200){
+    //                 this.product = res.data
+    //             }
+    //         })
+    //     },
+
+    // initCartlist(){
+    //     this.$http.get(`${serverUrl}`)
+    // }
+
+    async initCartlist() {
+      const result = await reqCartlist();
+      console.log(result);
+      //   this.products = result.data;
     },
-    mounted() {
-        
+    onSubmit() {
+      Notify({ type: "warning", message: "请输入地址" });
+      //   this.$router.replace("/address");
     },
-    beforeCreate() {}, 
-    beforeMount() {},
-    beforeUpdate() {}, 
-    updated() {}, 
-    beforeDestroy() {}, 
-    destroyed() {}, 
-    //如果页面有keep-alive缓存功能，这个函数会触发
-    activated() {}, 
-    }
+    // onClickIcon() {
+    //   Toast("点击图标");
+    // },
+    // onClickButton() {
+    //   Toast("点击按钮");
+    // },
+  },
+  created() {
+    this.initCartlist(); //初始数据
+  },
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+};
 </script>
-<style scoped>
-</style>
+<style scoped></style>
--- project/src/utils/request.js
+++ project/src/utils/request.js
@@ -1,37 +1,42 @@
 // 封装axios
-import axios from 'axios'
-import {getToken} from './auth'
+// import { Notify } from "vant";
 
-import {serverUrl} from './common'
+import axios from "axios";
+import { getToken } from "./auth";
+
+import { serverUrl } from "./common";
 
 const instance = axios.create({
-    baseURL:serverUrl, //url域名
-    timeout:5000 // 请求超时时间
+  baseURL: serverUrl, //url域名
+  timeout: 5000, // 请求超时时间
 });
 instance.interceptors.request.use(
-    function(config) {
-      if (getToken()) {
-        config.headers.authorization = "Bearer " + getToken();
-      }
-      return config;
-    },
-    function(error) {
-      return Promise.reject(error);
+  function(config) {
+    if (getToken()) {
+      config.headers.authorization = "Bearer " + getToken();
     }
-  );
-  
-  instance.interceptors.response.use(
-    function(response) {
-      return response;
-    },
-    function(error) {
-      return Promise.reject(error);
-    }
-  );
-  
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  function(error) {
+    // if (error && error.message.indexOf("401") > -1) {
+    //   Notify({ type: "danger", message: "你未授权，请先登录" });
+    //   location.href = "/#/login";
+    // }
+    return Promise.reject(error);
+  }
+);
 
 //get请求封装
-export const get =(url,params)=> instance.get(url,{params})
+export const get = (url, params) => instance.get(url, { params });
 
 //post请求封装
-export const post = (url,data)=>instance.post(url,data)
+export const post = (url, data) => instance.post(url, data);
