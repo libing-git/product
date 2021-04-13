d64b0a087aff458658b93f6d0c74aa7d889817f2
ice <787111212@qq.com>
2021-04-13 10:52:43 +0800

    logReg

--- project/src/components/content/login/login.vue
+++ project/src/components/content/login/login.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="login">
+    <Header></Header>
     <div class="login-form">
       <van-form @submit="onSubmit">
         <van-field
@@ -17,9 +18,23 @@
           placeholder="密码"
           :rules="[{ required: true, message: '请填写密码' }]"
         />
-        <div style="margin: 16px;">
-          <van-button round block type="info" native-type="submit"
-            >提交</van-button
+        <div style="margin: 16px">
+          <van-button
+            round
+            block
+            type="info"
+            native-type="submit"
+            class="logBtn"
+            >登录</van-button
+          >
+          <van-button
+            round
+            block
+            type="info"
+            native-type="button"
+            class="regBtn"
+            @click="toReg"
+            >注册</van-button
           >
         </div>
       </van-form>
@@ -31,9 +46,11 @@
 import { Notify } from "vant";
 // import store from '../store/user/index'
 import store from "../store";
-
+import Header from "../header/header";
 export default {
-  components: {},
+  components: {
+    Header,
+  },
   data() {
     return {
       username: "",
@@ -41,6 +58,9 @@ export default {
     };
   },
   methods: {
+    toReg() {
+      this.$router.push("/register");
+    },
     login() {
       localStorage.setItem("token", "xxxx");
       this.$router.push("/message");
@@ -75,6 +95,10 @@ export default {
 <style scoped>
 .login-form {
   width: 100%;
-  margin-top: 50%;
+  margin-top: 40%;
+}
+.logBtn,
+.regBtn {
+  margin-top: 20px;
 }
 </style>
